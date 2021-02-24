c271d60fbcc4c44bd064f4fa5aa4d82808f42eba
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-02-25 01:15:51 +0330

    add header of feed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import Feed from './components/Feed/Feed';
+import Widgets from './components/Widgets/Widgets';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-    <h1>lets build twitter clone</h1>
+    <div className="app">
     {/* Sidebar */}
-
     <Sidebar />
+
+    {/* Feed */}
+    <Feed />
+
+    {/* widgets */}
+    <Widgets />
     </div>
   );
 }
