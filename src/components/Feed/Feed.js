626b8bf7710cb1bc6033268184b4d5633b2138c2
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-02-25 18:14:14 +0330

    update feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,21 +1,37 @@
-import React from "react";
-import './Feed.css'
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
 import Post from "./Post/Post";
 import TweetBox from "./TweetBox/TweetBox";
+import Profile from "../../assets/images/profile.png";
+import db from "../../firebase/firebase";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
   return (
     <div className="feed">
       {/* Header */}
       <div className="feed__header">
         <h2>Home</h2>
       </div>
-      
-      {/* TweetBox */}
-        <TweetBox />
-      {/* Post */}
-        <Post />
 
+      {/* TweetBox */}
+      <TweetBox />
+      {posts.map((post) => (
+        <Post
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+        />
+      ))}
     </div>
   );
 }
