2d16143acad13ba00abde5d598611d85a19a8d0d
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-02-25 18:00:25 +0330

    firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBjBwIytoZHwMzNc2kt__bJdZzmBnjOGE8",
+    authDomain: "twitter-clone-db533.firebaseapp.com",
+    projectId: "twitter-clone-db533",
+    storageBucket: "twitter-clone-db533.appspot.com",
+    messagingSenderId: "941316632709",
+    appId: "1:941316632709:web:52afe224575ffe739c79ec",
+    measurementId: "G-61Y2KE0YE1",
+  };
+  
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  
+  const db = firebaseApp.firestore();
+  
+  export default db;
